767cd332a932b341c58e0c819bb2ce3ab1b3a0cb
Bruno <Bruno.Levy@inria.fr>
2024-01-21 09:36:44 +0100

    uses shifts instead of divisions whenever possible

--- humanshader.c
+++ humanshader.c
@@ -1,6 +1,6 @@
 // C version of humanshader
 // See https://humanshader.com/
-// (using a computer is not as fun, but it is interesting to have
+// (using a computer is clearly not as fun, but it is interesting to have
 //  a small not too computationally expensive raytracing program that
 //  can run on small softcores for PGAs).
 
@@ -8,8 +8,13 @@
 #define GL_height 40
 #include "GL_tty.h"
 
-// Note: on the worksheet on the webside, x|n means x/(10^n) (took me a while
-// to understand).
+// Note: on the worksheet on the webside, x|n means x/(10^n)
+// (it took me a while to understand).
+// Replaced most of them with shifts
+// /10    -> /8
+// /100   -> /128
+// /1000  -> /1024
+// /10000 -> /8192
 
 void human_shader(
     int x, int y, uint8_t* r_out, uint8_t* g_out, uint8_t* b_out
@@ -27,16 +32,16 @@ B:
     int R = 420;
     int B = 520;
     int t = 5000 + 8*h;
-    int p = (t*u)/100;
-    int q = (t*v)/100;
+    int p = (t*u) >> 7; 
+    int q = (t*v) >> 7; 
     int s = 2*q;
-    int w = ((1000+p-s)/100) + 8;
+    int w = ((1000+p-s)>>7) + 8;
     if(w > 0) R += w*w;
     int o = s + 2200;
-    R = (R*o)/10000;
-    B = (B*o)/10000;
+    R = (R*o) >> 13;
+    B = (B*o) >> 13;
     if(p > -q) {
-        w = (p+q)/10;
+        w = (p+q) >> 3;
         R += w;
         B += w;
     }
@@ -47,11 +52,11 @@ C:
     p = h + 8*v*v;
     int c = -240*v-p;
     if(c > 1200) {
-        o = (6*c)/10;
+        o = (6*c)/10;     // keep /10 here
         o = c*(1500-o);
-        o = (o/100)-8360;
-        R = (R*o)/1000;
-        B = (B*o)/1000;
+        o = (o/100)-8360; // keep /100 here
+        R = (R*o) >> 10;
+        B = (B*o) >> 10;
     }
     int r = c + u*v;
     int d = 3200 - h - 2*r;
@@ -65,7 +70,7 @@ D:
 E:
     if(R > 255) R = 255;
     if(B > 255) B = 255;
-    int G = (7*R+3*B)/10;
+    int G = (7*R+3*B)/10; // Keep /10 here
     *r_out = (uint8_t)R;
     *g_out = (uint8_t)G;
     *b_out = (uint8_t)B;
