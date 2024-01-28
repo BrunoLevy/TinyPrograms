00b9f80f27c359677f32a1934856cce33f80d433
Bruno <Bruno.Levy@inria.fr>
2024-01-28 15:04:50 +0100

    aesthetic changes in render.c

--- render.c
+++ render.c
@@ -1,25 +1,31 @@
 // render.c
-// a port of Dmitry Sokolov's 70 lines raytracer
+// a port in C of Dmitry Sokolov's 70 lines raytracer (initially in Python)
 
-#define GL_width  80
-#define GL_height 50
+#define GL_width  80 // You can increase resolution if you have a large tty
+#define GL_height 50 // (then you need to decrease focal below
+float focal = 60;    // focal length (decrease to zoom-in, increase to zoom-out)
+#define DIFFUSE 1    // Enables partly diffuse materials (but costs a lot, needs multiple rays)
+int nrays = 1 + DIFFUSE * 100; // Number of rays used to sample diffuse BRDF.
+float roughness = 1.0/6.0;     // The larger, the more diffuse, the smaller, the more specular
+int maxdepth = 3;   // maximum number of reflexions
+
 #include "GL_tty.h"
 #include <math.h>
 #include <stdbool.h>
 #include <string.h>
 
-static inline void vcopy(float* to, const float* from) { memcpy(to, from, sizeof(float)*3); }
-static inline float dot(const float* u, const float* v) { return u[0]*v[0]+u[1]*v[1]+u[2]*v[2]; }
-static inline float distance2(const float* p1, const float* p2) {
-    return (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]) + (p1[2]-p2[2])*(p1[2]-p2[2]);
-}
+float ambient_color[3] = { .5, .5, .5  };  
+float light_color[3]   = {1.0, 1.0, 1.0};
+float azimuth = 30.*M_PI/180.;
+
+static inline void vcopy(float* to, const float* from)          { memcpy(to, from, sizeof(float)*3);    }
+static inline float dot(const float* u, const float* v)         { return u[0]*v[0]+u[1]*v[1]+u[2]*v[2]; }
+static inline float distance2(const float* p1, const float* p2) { return (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]) + (p1[2]-p2[2])*(p1[2]-p2[2]); }
 #define VEC(V) V[c]
 #define VECOP(E) for(int c=0; c<3; ++c) E
+#define NORMALIZE(V) { float s = 1.0/sqrt(dot(V,V)); VECOP(VEC(V) *= s); }
 
-bool box_intersect(
-    const float* bmin, const float* bmax, const float* ray_origin, const float* ray_direction,
-    float* point, float* normal
-) {
+bool box_intersect(float* point, float* normal, const float* bmin, const float* bmax, const float* ray_origin, const float* ray_direction) {
     for(int i=0; i<3; ++i) { // for each coordinate axis
         if(fabs(ray_direction[i]) < 1e-3) continue; // avoid divide by 0
         normal[0] = normal[1] = normal[2] = 0.0;    // here we test against 3 planes (instead of 6), i.e.
@@ -34,10 +40,7 @@ bool box_intersect(
     return false;
 }
 
-bool sphere_intersect(
-    const float* center, float radius, const float* ray_origin, const float* ray_direction,
-    float* point, float* normal
-) {
+bool sphere_intersect(float* point, float* normal, const float* center, float radius, const float* ray_origin, const float* ray_direction) {
     float V[3] = { center[0]-ray_origin[0], center[1]-ray_origin[1], center[2]-ray_origin[2] };
     float proj = dot(ray_direction, V);
     float delta = radius*radius + proj*proj - dot(V,V);
@@ -52,10 +55,7 @@ bool sphere_intersect(
     return false;
 }
 
-bool scene_intersect(
-    const float* ray_origin, const float* ray_direction,
-    float* point, float* normal, float* color
-) {
+bool scene_intersect(float* point, float* normal, float* color, const float* ray_origin, const float* ray_direction) {
     #define NOBJ 5
     static struct { float color[3]; float p1[3]; float p2[3];  float radius; } O[NOBJ] = {
         { {1.,.4,.6}, {6,0,7     }, {0,0,0    }, 2   },
@@ -71,9 +71,9 @@ bool scene_intersect(
     for(int o=0; o<NOBJ; ++o) {
         bool has_isect = false;
         if(O[o].radius == 0.0) {
-            has_isect = box_intersect(O[o].p1, O[o].p2, ray_origin, ray_direction, p, N);
+            has_isect = box_intersect(p, N, O[o].p1, O[o].p2, ray_origin, ray_direction);
         } else {
-            has_isect = sphere_intersect(O[o].p1, O[o].radius, ray_origin, ray_direction, p, N);
+            has_isect = sphere_intersect(p, N, O[o].p1, O[o].radius, ray_origin, ray_direction);
         }
         if(has_isect) {
             scene_has_isect = true;
@@ -85,47 +85,40 @@ bool scene_intersect(
 }
 
 float urand() { return 2.0*((float)(random() & 65535)/65535.0) - 1.0; }
-
 void reflect(const float* I, const float* N, float* R) {
-    float w = 2*dot(I,N);
-    VECOP(VEC(R) = VEC(I) - w*VEC(N) + urand() / 6.0);
-    float l = sqrt(dot(R,R));
-    VECOP(VEC(R) *= l);
+    float w = 2.*dot(I,N);
+#if DIFFUSE == 1   
+    VECOP(VEC(R) = VEC(I) - w*VEC(N) + urand() * roughness);
+#else
+    VECOP(VEC(R) = VEC(I) - w*VEC(N));
+#endif
+    NORMALIZE(R);
 }
 
-float ambient_color[3] = { .5, .5, .5 };
-float light_color[3] = {1.0, 1.0, 1.0};
-float focal = 60; float azimuth = 30.*M_PI/180.;
-int nrays = 10; int maxdepth = 3;
-
-void trace(const float* eye, const float* ray, int depth, int maxdepth, float* rgb) {
+void trace(float* rgb, const float* eye, const float* ray, int depth, int maxdepth) {
     if(depth > maxdepth) { vcopy(rgb, ambient_color); return; }
     float point[3]; float normal[3]; float color[3];
-    if(!scene_intersect(eye, ray, point, normal, color)) { vcopy(rgb, ambient_color); return; }
+    if(!scene_intersect(point, normal, color, eye, ray)) { vcopy(rgb, ambient_color); return; }
     if(color[0] > 1.0) { vcopy(rgb, light_color); return; } // if we hit a lamp -> white
     float color2[3], ray2[3];
     reflect(ray, normal, ray2);
-    trace(point, ray2, depth+1, maxdepth, color2);
+    trace(color2, point, ray2, depth+1, maxdepth);
     VECOP(VEC(rgb) = VEC(color) * VEC(color2));
 }
 
 void render(int X, int Y, float* r, float* g, float *b) {
     static float zero[3] = {0,0,0};
     float rgb[3] = { 0.0, 0.0, 0.0 };
-    float ray[3] = { (float)X-(float)GL_width/2., (float)Y-(float)GL_height/2., focal};
-    float s = 1.0/sqrt(dot(ray,ray));
-    VECOP(VEC(ray) = s * VEC(ray));
+    float ray[3] = { (float)X-(float)GL_width/2., (float)Y-(float)GL_height/2., focal};  NORMALIZE(ray);
     float x =  cos(azimuth)*ray[0] + sin(azimuth)*ray[2]; // rotate the ray 30 degrees around Y-axis
     float z = -sin(azimuth)*ray[0] + cos(azimuth)*ray[2]; 
     ray[0] = x; ray[2] = z;
     for(int r=0; r<nrays; ++r) {
         float rgb_ray[3];
-        trace(zero, ray, 0, maxdepth, rgb_ray);
+        trace(rgb_ray, zero, ray, 0, maxdepth);
         VECOP(VEC(rgb) += VEC(rgb_ray));
     }
-    *r = rgb[0] / (float)nrays;
-    *g = rgb[1] / (float)nrays;
-    *b = rgb[2] / (float)nrays;
+    *r = rgb[0] / (float)nrays; *g = rgb[1] / (float)nrays; *b = rgb[2] / (float)nrays;
 }
 
 int main() {
